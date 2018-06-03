5345557c5eec5c07eba30e7665cd1594a66d536c
Solusverd22 <32770672+Solusverd22@users.noreply.github.com>
2018-06-04 00:14:43 +0100

    Added very basic movement, wasd

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 
-	public Rigidbody myRigid;
+	private Rigidbody myRigid;
+
+	private float mouseSensitivity = 10;
+	private float targetAngle;
 
 	// Use this for initialization
 	void Start () {
-		
+		PlayerPrefs.GetFloat("mouseSensitivity", mouseSensitivity);
+		myRigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log(mouseSensitivity);
 		myRigid.AddRelativeForce(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"),ForceMode.Impulse);
 	}
 }
