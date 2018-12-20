d2507a6a0ac2702424cbaee12ed9688f52bd916f
Solusverd22 <32770672+Solusverd22@users.noreply.github.com>
2018-12-20 07:12:16 +0000

    GOT IK WORKING
    :weary: :weary: :weary:

---
+++ Assets/Scripts/FrogIK.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FrogIK : MonoBehaviour {
+
+	#region Declaration
+	[SerializeField]
+	Transform _RFootTrans;
+
+	Animator animator;
+	#endregion
+
+	void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
+	void OnAnimatorIK() {
+		animator.SetIKPosition(AvatarIKGoal.LeftFoot,Vector3.zero);
+		animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+	}
+}
